8ad3d40c970aa8edae24c14610fa0c967be07e4a
sfcarz <chrisarojasz2003@gmail.com>
2020-09-25 16:10:39 -0700

    Fist Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer')
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+// console.log(inquirer);
+// console.log(axios);
+console.log(path);
